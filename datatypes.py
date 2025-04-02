0c5150acbf5feedc206f0c87212730a6ece3ee47
Reyansh-R <reyansh.rushindra@gmail.com>
2025-04-02 16:53:22 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+a = 8.888
+print("datatype of a:",type(a))
+x = int(a)
+print("datatype of x:",type(x))
+b = 7
+print("datatype of b:",type(b))
+c = "I_like_Macaroni"
+print("datatype of c:",type(c))
+d = True 
+print("datatype of d:",type(d))
+y = float(b)
+print("datatype of y:",type(y))
+
+g = "codingle"
+z = reversed(g)
+print(z)
+
+text = str(input("Enter a string: "))
+revText = text[::-1]
+text = revText
+print("Reverse of given string is ")
+print(text)
+
+Message = "noice"
+print(Message.upper())e
+print(Message.lower())
+
+str1 = "deadpool"
+str2 = "wolverine"
+print('str1 + str2 =', str1 + str2)

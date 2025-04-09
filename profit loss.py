0a221468821a8ed1ddb569d8c7fd6fff5aeb135d
Reyansh-R <reyansh.rushindra@gmail.com>
2025-04-09 16:51:25 +0530

    Add files via upload

---
+++ profit loss.py
@@ -0,0 +1,12 @@
+print("A man has bought oranges")
+CP = int(input("enter the price of the oranges : "))
+print("The man then sold the oranges")
+SP = int(input("enter the amount of money he got after selling the oranges :"))
+if (SP > CP):
+    P = SP - CP
+    print("The man has made profit of : ",P)
+else:
+    print("The man has made a loss")
+
+
+ 

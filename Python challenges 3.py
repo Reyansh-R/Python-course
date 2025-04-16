4ce6104cc8cf0c10713c5906e8a94518429e2d0b
Reyansh-R <reyansh.rushindra@gmail.com>
2025-04-16 16:44:55 +0530

    Add files via upload

---
+++ Python challenges 3.py
@@ -0,0 +1,11 @@
+mean1 = 38
+correctnumber = 56
+wrongnumber = 36
+totalnumber = 40
+sum = mean1*totalnumber
+print("The sum of 40 number: ",sum)
+num2 = sum - ((wrongnumber)-(correctnumber))
+print("sum - ((wrongnumber)-(correctnumber)): ",num2)
+
+mean2 = num2/totalnumber
+print(mean2)

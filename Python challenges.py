74f52cbff6581649533d0f90efa03bb2e587465f
Reyansh-R <reyansh.rushindra@gmail.com>
2025-04-16 16:27:57 +0530

    Add files via upload

---
+++ Python challenges.py
@@ -0,0 +1,14 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v + w) * v / y;
+print("Value of (v + w) * v / y; is",z)
+
+name = "Alex"
+age = 0 
+if name == "Alex" or name == "John" and age >= 2 :
+    print("Hello welcome")
+else:
+    print("Bye Bye")

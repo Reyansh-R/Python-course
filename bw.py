4f4c3c62dc248bc9601a788829feac32a94873b4
Reyansh-R <reyansh.rushindra@gmail.com>
2025-10-09 19:14:05 +0530

    Add files via upload

---
+++ bw.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 4
+
+print("num1 and num2 are", num1 & num2)
+
+print("num1 and num2 are", num1 | num2)
+
+print("num1 and num2 are", ~num1)
+
+print("num1 and num2 are", num1 ^ num2)
+
+print("num1 << 1=", num1 << 1)
+print("num2 << 1=", num2 << 1)
+

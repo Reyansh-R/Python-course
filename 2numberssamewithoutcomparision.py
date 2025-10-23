36bb2885e8de650db04d2c98fe743529ae0b57b4
Reyansh-R <reyansh.rushindra@gmail.com>
2025-10-23 19:15:12 +0530

    Add files via upload

---
+++ 2numberssamewithoutcomparision.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter a number: "))
+
+if ((number1 ^ number2) != 0):
+    print("they are not equal")
+else:
+    print("They are equal")
+    
---
+++ oddoccuringn.py
@@ -0,0 +1,17 @@
+def oddocurringnumber(arr):
+
+    res = 0
+
+    for element in arr:
+        res = res ^ element
+    return res
+
+arr = []
+n = int(input("Enter array size: "))
+
+while(n):
+    num = int(input("Enter a number: "))
+    arr.append(num)
+    n -= 1
+
+print("The odd occuring number is: ", oddocurringnumber(arr))
